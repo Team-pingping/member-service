b19c60573520284bc9d61f1b3b50606ded5949cc
codrin2 <hssang127@gmail.com>
2024-10-08 21:51:41 +0900

    [feat] #1 User Service 구현

--- src/main/kotlin/com/pingping/global/exception/ExceptionContent.kt
+++ src/main/kotlin/com/pingping/global/exception/ExceptionContent.kt
@@ -4,7 +4,5 @@ import org.springframework.http.HttpStatus
 
 enum class ExceptionContent(val httpStatus: HttpStatus, val message: String) {
 
-    // 예시 예외 수정 필요
-    BAD_REQUEST_STAGE_STATUS_TYPE(HttpStatus.BAD_REQUEST, "잘못된 요청입니다. 유효하지 않은 채용 상태입니다. (준비중, 합격, 불합격으로 입력해주세요.)"),
-    NO_VALID_STAGE_FOUND(HttpStatus.NOT_FOUND, "채용 일정에 채용 전형이 존재하지 않습니다.")
+    USER_NOT_FOUND(HttpStatus.NOT_FOUND, "해당 사용자를 찾을 수 없습니다.")
 }
